6614d96999eecf96810af266ea9106889ad7dc4a
limlz <62799391+limlz@users.noreply.github.com>
2021-11-07 23:35:39 +0800

    edited stuff

--- main.py
+++ main.py
@@ -1,5 +1,56 @@
 from flask import Flask, render_template, g
 import sqlite3
+import hashlib
+import json
+from time import time
+
+
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.pending_transactions = []
+
+        self.new_block(previous_hash="The Times 03/Jan/2009 Chancellor on brink of second bailout for banks.", proof = 100)
+
+    def new_block(self, proof, previous_hash=None):
+        block = {
+            "index": len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.pending_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+        self.pending_transactions = []
+        self.chain.append(block)
+
+        return block
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+    def new_transaction(self, sender, recipient, amount):
+        transaction = {
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        }
+        self.pending_transactions.append(transaction)
+        return self.last_block['index'] + 1
+
+    def hash(self, block):
+        string_object = json.dumps(block, sort_keys=True)
+        block_string = string_object.encode()
+
+        raw_hash = hashlib.sha256(block_string)
+        hex_hash = raw_hash.hexdigest()
+
+        return hex_hash
+
+
+blockchain = Blockchain()
+
+
 
 app = Flask(__name__)
 with app.app_context():
@@ -51,6 +102,11 @@ with app.app_context():
     @app.route('/passwordreset',methods=['GET', 'POST'])
     def passwordreset():
         return render_template('passwordreset.html')
+
+    @app.route('/submission',methods=['GET', 'POST'])
+    def blockchain():
+
+        return render_template('submission.html')
 
     if __name__ == "__main__":
         app.run()
