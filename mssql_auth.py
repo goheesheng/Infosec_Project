8273ef0b2abb3de72768c0af1722ef76c3487e65
limlz <62799391+limlz@users.noreply.github.com>
2021-12-06 08:05:53 +0800

    Revert "fixed login page"
    This reverts commit c7c4288daec7d0c5cc55238804f466b39eca5d04.

--- mssql_auth.py
+++ mssql_auth.py
@@ -1,5 +1,5 @@
 #ENTER YOUR OWN SERVER CONNECTION FOR MSSQLaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 # server = 'Desktop-75MSPGF'
-server = 'DESKTOP-DMO8V7F\SQLEXPRESS'
+server = 'GOHDESKTOP\SQLEXPRESS'
 
 database = 'database1'
