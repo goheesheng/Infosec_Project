5cd5e1d1a197641b5a5170d8291bb67c70beb8bc
Kae <kaelantang2@gmail.com>
2021-12-09 20:25:04 +0800

    made the addition of appointment to the patient dynamic

--- mssql_auth.py
+++ mssql_auth.py
@@ -1,5 +1,5 @@
 #ENTER YOUR OWN SERVER CONNECTION FOR MSSQLaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 # server = 'Desktop-75MSPGF'
-server = 'GOHDESKTOP\SQLEXPRESS'
-
+#server = 'GOHDESKTOP\SQLEXPRESS'
+server = 'LAPTOP-3MCBQP3T\SQLEXPRESS'
 database = 'database1'
