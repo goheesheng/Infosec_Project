927e89ba26d034cbf0d111162cdf58bfd9aab34f
Quidproquo <goheesheng@hotmail.com>
2021-12-10 19:38:35 +0800

    Revert "Merge branch 'main' of https://github.com/goheesheng/Infosec_Project"
    This reverts commit 00443307df130904007faeb0701cfe7b9ab2eea0, reversing
    changes made to 90e8a483d164bb176c3e1cf2504b28461c7235c0.

--- database1.sql
+++ database1.sql
@@ -92,8 +92,6 @@ CREATE TABLE [dbo].[hr](
 	[last_name] [nchar](20) NOT NULL,
 	[pass_hash] [varchar](50) NOT NULL,
 	[otp_code] [varchar](50) NOT NULL,
-	[address] [varchar](50) NOT NULL,
-	[postal_code] [varchar](6) NOT NULL,
 	[email] [varchar](50) NOT NULL
 ) ON [PRIMARY]
 GO
@@ -110,8 +108,6 @@ CREATE TABLE [dbo].[doctors](
 	[pass_hash] [varchar](50) NOT NULL,
 	[otp_code] [varchar](50) NOT NULL,
 	[email] [varchar](50) NOT NULL,
-	[address] [varchar](50) NOT NULL,
-	[postal_code] [varchar](6) NOT NULL,
 	[department] [varchar](50) NOT NULL
 ) ON [PRIMARY]
 GO
@@ -158,8 +154,6 @@ CREATE TABLE [dbo].[patients](
 	[pass_hash] [varchar](50) NOT NULL,
 	[otp_code] [varchar](50) NOT NULL,
 	[email] [varchar](50) NOT NULL,
-	[address] [varchar](50) NOT NULL,
-	[postal_code] [varchar](6) NOT NULL,
 	[hospital] [varchar](50) NULL,
 	[tending_physician] [varchar](50) NULL,
 	[appointment][varchar](20) NULL,
@@ -177,8 +171,6 @@ CREATE TABLE [dbo].[researchers](
 	[pass_hash] [varchar](50) NOT NULL,
 	[otp_code] [varchar](50) NOT NULL,
 	[email] [varchar](50) NOT NULL,
-	[address] [varchar](50) NOT NULL,
-	[postal_code] [varchar](6) NOT NULL,
 	[company] [varchar](50) NULL,
 	[researcher_id] [int] IDENTITY(1,1) NOT NULL
 ) ON [PRIMARY]
